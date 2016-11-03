61e07fc850b2441ef76d429ebf9eca0db648bc10
Necromoni <necromoni@gmail.com>
2016-11-03 01:39:16 -0700

    toonbase: Add color and settings globals for future use

---
+++ toontown/toonbase/ColorGlobals.py
@@ -0,0 +1,8 @@
+from panda3d.core import Vec4
+
+CDefault = Vec4(1.0, 1.0, 1.0, 1.0)
+CToontownBlue = Vec4(0.23, 0.66, 0.99, 1)
+CGray = Vec4(0.6, 0.6, 0.6, 1)
+CLime = Vec4(0.62, 0.98, 0.219, 1)
+CBlack = Vec4(0.0, 0.0, 0.0, 1.0)
+COrange = Vec4(1.0, 0.6, 0.0, 1.0)
---
+++ toontown/toonbase/SettingsGlobals.py
@@ -0,0 +1,9 @@
+Music = 'music'
+MusicVolume = 'music-volume'
+Sound = 'sfx'
+SoundVolume = 'sound-volume'
+WantWhispers = 'want-whispers'
+WantFriendWhispers = 'want-friend-whispers'
+WantNonFriendWhispers = 'acceptingNonFriendWhispers'  # Dict of toonId to boolean
+WantFriends = 'want-friends'
+
