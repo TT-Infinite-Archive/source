c36a07dc52754c140e97a1d5af5d687f4da23edd
Necromoni <necromoni@gmail.com>
2016-11-21 20:58:58 -0800

    battle: Remove name attribute from effects

--- toontown/battle/Effect.py
+++ toontown/battle/Effect.py
@@ -1,16 +1,24 @@
 class Effect:
-    def __init__(self, uid, name):
+    def __init__(self, uid):
         self.uid = uid
-        self.name = name
 
     def applyTo(self, av):
         pass
 
 
 class HealEffect(Effect):
-    def __init__(self, uid, name, amount):
-        Effect.__init__(self, uid, name)
+    def __init__(self, uid, amount):
+        Effect.__init__(self, uid)
         self.amount = amount
 
     def applyTo(self, av):
         av.toonUp(self.amount)
+
+
+class DamageEffect(Effect):
+    def __init__(self, uid, amount):
+        Effect.__init__(self, uid)
+        self.amount = amount
+
+    def applyTo(self, av):
+        av.takeDamage(self.amount)
