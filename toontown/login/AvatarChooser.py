8bbc14790ab83527ec7a2a7d20ec1fcc5cd33ce7
Markgasus <markgasus@gmail.com>
2016-11-29 14:32:31 -0500

    login: Move disconnect button to the left

--- toontown/login/AvatarChooser.py
+++ toontown/login/AvatarChooser.py
@@ -115,8 +115,8 @@ class AvatarChooser(StateData.StateData):
             text_pos=TTLocalizer.ACdisconnectButtonPos,
             text_scale=TTLocalizer.ACdisconnectButton, image_scale=1,
             image1_scale=1.05, image2_scale=1.05, scale=1.05,
-            pos=(-0.25, 0, 0.075), command = self.__back)
-        self.disconnectButton.reparentTo(base.a2dBottomRight)
+            pos=(0.25, 0, 0.075), command = self.__back)
+        self.disconnectButton.reparentTo(base.a2dBottomLeft)
 
         """
         self.logoutButton = DirectButton(
