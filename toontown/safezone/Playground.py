11ecd0578a714e6bfe7b161db9fe693f11dc6b1d
Necromoni <necromoni@gmail.com>
2016-11-30 20:37:35 -0800

    safezone: Fix crash when leaving playground and music is disabled

--- toontown/safezone/Playground.py
+++ toontown/safezone/Playground.py
@@ -204,6 +204,10 @@ class Playground(Place.Place):
         if self.loader.music:
             base.playMusic(self.loader.music, looping=1, volume=0.8)
 
+    def stopMusic(self):
+        if self.loader.music:
+            self.loader.music.stop()
+
     def enter(self, requestStatus):
         self.fsm.enterInitialState()
         messenger.send('enterPlayground')
@@ -307,7 +311,7 @@ class Playground(Place.Place):
             self.loader.exitAnimatedProps(i)
 
         self.loader.hood.stopSky()
-        self.loader.music.stop()
+        self.stopMusic()
 
     def load(self):
         Place.Place.load(self)
