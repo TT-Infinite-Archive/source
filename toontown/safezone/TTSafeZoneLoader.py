122a6f73b997de290425b5020fa20a573b384054
Necromoni <necromoni@gmail.com>
2016-11-30 20:23:43 -0800

    safezone: Wrong way

--- toontown/safezone/TTSafeZoneLoader.py
+++ toontown/safezone/TTSafeZoneLoader.py
@@ -19,12 +19,11 @@ class TTSafeZoneLoader(SafeZoneLoader.SafeZoneLoader):
         self.safeZoneStorageDNAFile = 'phase_4/dna/storage_TT_sz.pdna'
 
     def load(self):
-        if base.config.GetBool('want-ttc-jukebox', False):
-            # The load method loads music, we dont want music if we have a jukebox that plays music for us
+        if not base.config.GetBool('want-ttc-jukebox', False):
+            # The load method loads music, we want music if don't we have a jukebox that plays music for us
             SafeZoneLoader.SafeZoneLoader.load(self)
         else:
-            # Do the other things in the load function that isn't playing music, this has to be updated
-            # if we change the overridden func
+            # Don't play music if we have a jukebox, but do the other things in the loader
             self.createSafeZone(self.dnaFile)
             self.parentFSMState.addChild(self.fsm)
         self.birdSound = map(base.loadSfx, ['phase_4/audio/sfx/SZ_TC_bird1.ogg',
