2a397164a589fc20a9ebf6962555e471263bfcc3
Markgasus <markgasus@gmail.com>
2016-11-30 22:04:11 -0500

    safezone: Blur background when interacting with the jukebox

--- toontown/safezone/DistributedJukebox.py
+++ toontown/safezone/DistributedJukebox.py
@@ -6,7 +6,9 @@ from toontown.safezone import JukeboxGlobals
 import random
 from toontown.util.VolumeInterval import VolumeInterval
 from toontown.toontowngui.JukeboxGui import JukeboxGui
+from direct.filter.CommonFilters import CommonFilters
 
+filters = CommonFilters(base.win, base.cam)
 
 class DistributedJukebox(DistributedObject):
     notify = directNotify.newCategory('DistributedJukebox')
@@ -81,9 +83,16 @@ class DistributedJukebox(DistributedObject):
         self.gui.show()
         base.cr.playGame.getPlace().setState('purchase')
 
+        # Blur the background when interacting with the jukebox
+        filters.setBlurSharpen(0)
+
     def exitGui(self):
         self.gui.hide()
         base.cr.playGame.getPlace().setState('walk')
+
+        # Remove the blur when the user is done with the jukebox
+        filters.setBlurSharpen(1)
+        filters.delBlurSharpen()
 
     def d_requestPlaySong(self, songId):
         self.notify.debug('Sending request to play song %s' % songId)
