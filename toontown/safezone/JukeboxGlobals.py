5816b864aea849b6bd567de0bd55bdad123247e1
Markgasus <markgasus@gmail.com>
2016-11-30 23:11:52 -0500

    safezone: Start adding some songs to the jukebox

--- toontown/safezone/JukeboxGlobals.py
+++ toontown/safezone/JukeboxGlobals.py
@@ -17,8 +17,10 @@ class ToontownSong:
 
 Songs = {
     0: None,
-    1: ToontownSong(1, TTLocalizer.MusicTcNbrhood, 'phase_4/audio/bgm/TC_nbrhood.ogg', 58),
-    2: ToontownSong(2, TTLocalizer.MusicDdNbrhood, 'phase_6/audio/bgm/DD_SZ.ogg', 32)
+    1: ToontownSong(1, TTLocalizer.MusicThemeSong, 'phase_3/audio/bgm/tti_theme.ogg', 90),
+    2: ToontownSong(2, TTLocalizer.MusicMakeAToon, 'phase_3/audio/bgm/create_a_toon.ogg', 175),
+    3: ToontownSong(3, TTLocalizer.MusicTcNbrhood, 'phase_4/audio/bgm/TC_nbrhood.ogg', 58),
+    4: ToontownSong(4, TTLocalizer.MusicDdNbrhood, 'phase_6/audio/bgm/DD_SZ.ogg', 32)
 }
 
 FadeTime = 5
