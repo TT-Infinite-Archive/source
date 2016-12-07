41b32cea15a1e5f2dec1c5010b66c6182614232b
Necromoni <necromoni@gmail.com>
2016-12-06 23:30:33 -0800

    avatar: Truly show how much hp you lost

--- otp/avatar/DistributedAvatar.py
+++ otp/avatar/DistributedAvatar.py
@@ -112,7 +112,6 @@ class DistributedAvatar(DistributedActor, Avatar):
             return
         oldHp = self.hp
         self.hp = max(self.hp - hpLost, 0)
-        hpLost = oldHp - self.hp
         if hpLost > 0:
             self.showHpText(-hpLost, bonus)
             self.hpChange(quietly=0)
