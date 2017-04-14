c309f409706e8c32b8006a4fbabb571395416b6d
Markgasus <markgasus@gmail.com>
2017-04-14 02:20:41 -0400

    Environment Update
    Load destroyed buildings through the code rather than dna
    Add new skybox
    Add rain
    Improve code quality

--- toontown/hood/SZHood.py
+++ toontown/hood/SZHood.py
@@ -4,6 +4,8 @@ from toontown.safezone.SZSafeZoneLoader import SZSafeZoneLoader
 from toontown.toonbase import ToontownGlobals
 from toontown.hood.ToonHood import ToonHood
 from otp.otpbase.OTPGlobals import DefaultCameraFov
+from panda3d.core import Vec4, Filename
+from toontown.battle import BattleParticles
 
 class SZHood(ToonHood):
     notify = directNotify.newCategory('SZHood')
@@ -13,32 +15,71 @@ class SZHood(ToonHood):
     STORAGE_DNA = 'phase_6/dna/storage_SZ.pdna'
     SKY_FILE = 'phase_3.5/models/props/TT_sky'
     SPOOKY_SKY_FILE = 'phase_3.5/models/props/BR_sky'
-    TITLE_COLOR = (0.38, 0.79, 0.31, 1.0)
+    TITLE_COLOR = (0.5, 0.5, 0.5, 1.0)
+    NIGHTSKY_FILE = None
+    SUNSKY_FILE = None
 
     HOLIDAY_DNA = {}
 
     def __init__(self, parentFSM, doneEvent, dnaStore, hoodId):
         ToonHood.__init__(self, parentFSM, doneEvent, dnaStore, hoodId)
 
-    def startSky(self):
-        ToonHood.startSky(self)
-        self.sky.setScale(4.0)
-
-    def enter(self, requestStatus):
-        ToonHood.enter(self, requestStatus)
-        base.localAvatar.setCameraFov(ToontownGlobals.CogHQCameraFov)
-
-        render.setColorScale(Vec4(0.55, 0.35, 0.35, 1))
-
-        base.cr.shardTimeManager.setCurrentTime(1000)
-
-    def processTime(self):
-        pass
-
-    def exit(self):
-        ToonHood.exit(self)
-        base.localAvatar.setCameraFov(DefaultCameraFov)
+        self.nightSkyFile = self.NIGHTSKY_FILE
+        self.sunSkyFile = self.SUNSKY_FILE
+        self.titleColor = self.TITLE_COLOR
+        self.rain = None
+        self.rainRender = None
 
     def load(self):
         ToonHood.load(self)
         self.fog = Fog('SZFog')
+        self.startRain()
+        base.localAvatar.setCameraFov(ToontownGlobals.CogHQCameraFov)
+        base.camLens.setNearFar(ToontownGlobals.StrikeZoneCameraNear, ToontownGlobals.StrikeZoneCameraFar)
+
+        render.setColorScale(Vec4(0.55, 0.35, 0.35, 1))
+        self.sky.setScale(3)
+
+        if __debug__:
+            skyblue2Filename = Filename('../resources/phase_3.5/maps/skyblue2_invasion.jpg')
+            middayskyBFilename = Filename('../resources/phase_3.5/maps/middayskyB_invasion.jpg')
+            toontown_central_tutorial_palette_4amla_1Filename = Filename(
+                '../resources/phase_3.5/maps/toontown_central_tutorial_palette_4amla_1_invasion.jpg')
+            toontown_central_tutorial_palette_4amla_1_aFilename = Filename(
+                '../resources/phase_3.5/maps/toontown_central_tutorial_palette_4amla_1_a_invasion.rgb')
+        else:
+            skyblue2Filename = Filename('/phase_3.5/maps/skyblue2_invasion.jpg')
+            middayskyBFilename = Filename('/phase_3.5/maps/middayskyB_invasion.jpg')
+            toontown_central_tutorial_palette_4amla_1Filename = Filename(
+                '/phase_3.5/maps/toontown_central_tutorial_palette_4amla_1_invasion.jpg')
+            toontown_central_tutorial_palette_4amla_1_aFilename = Filename(
+                '/phase_3.5/maps/toontown_central_tutorial_palette_4amla_1_a_invasion.rgb')
+
+        self.sky.findTexture('skyblue2').read(skyblue2Filename)
+        self.sky.findTexture('middayskyB').read(middayskyBFilename)
+        self.sky.findTexture('toontown_central_tutorial_palette_4amla_1').read(
+            toontown_central_tutorial_palette_4amla_1Filename, toontown_central_tutorial_palette_4amla_1_aFilename, 0,
+            0)
+
+    def unload(self):
+        self.stopRain()
+        del self.rain
+        del self.rainRender
+
+        ToonHood.exit(self)
+        base.localAvatar.setCameraFov(DefaultCameraFov)
+
+    def startRain(self):
+        self.rain = BattleParticles.loadParticleFile('raindisk.ptf')
+        self.rain.setPos(0, 0, 20)
+        self.rainRender = render.attachNewNode('rainRender')
+        self.rainRender.setDepthWrite(0)
+        self.rainRender.setBin('fixed', 1)
+        self.rain.start(camera, self.rainRender)
+
+    def stopRain(self):
+        if self.rain:
+            self.rain.cleanup()
+
+    def processTime(self):
+        pass
--- toontown/safezone/SZPlayground.py
+++ toontown/safezone/SZPlayground.py
@@ -28,7 +28,35 @@ class SZPlayground(Playground.Playground):
             dnaStorage.cleanup()
             base.cr.sendSetZoneMsg(self.zoneId, zoneVisDict.values()[0])
 
+            self.loadDestroyedBuildings()
+
+    def loadDestroyedBuildings(self):
+        self.toonHall = loader.loadModel('phase_4/models/corpstrike/destroyed_toonhall')
+        self.toonHall.reparentTo(render)
+        self.toonHall.setPos(116.66, 24.29, 4)
+        self.toonHall.setHpr(-90, 0, 0)
+
+        self.bank = loader.loadModel('phase_4/models/corpstrike/destroyed_bank')
+        self.bank.reparentTo(render)
+        self.bank.setPos(57.1796, 38.6656, 0.3)
+
+        self.library = loader.loadModel('phase_4/models/corpstrike/destroyed_library')
+        self.library.reparentTo(render)
+        self.library.setPos(91.4475, -44.9255, 4)
+        self.library.setHpr(180, 0, 0)
+
+    def unloadDestroyedBuildings(self):
+        self.toonHall.removeNode()
+        del self.toonHall
+
+        self.bank.removeNode()
+        del self.bank
+
+        self.library.removeNode()
+        del self.library
+
     def exit(self):
         Playground.Playground.exit(self)
+        self.unloadDestroyedBuildings()
 
     dnaStorage = DNAStorage()
