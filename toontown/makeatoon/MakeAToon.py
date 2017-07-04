281669720f6c5c3a38f91e9c20f47deaf046ae3d
Drew <drewgamesaw@gmail.com>
2017-07-04 11:38:22 -0400

    MAT: This song is now being used on main menu, so put the old song back here.

--- toontown/makeatoon/MakeAToon.py
+++ toontown/makeatoon/MakeAToon.py
@@ -293,7 +293,7 @@ class MakeAToon(StateData.StateData):
         self.cos.load()
         self.cls.load()
         self.ns.load()
-        self.music = base.loadMusic('phase_3/audio/bgm/tti_pick_a_toon_theme.ogg')
+        self.music = base.loadMusic('phase_3/audio/bgm/create_a_toon.ogg')
         self.musicVolume = base.config.GetFloat('makeatoon-music-volume', 1)
         self.sfxVolume = base.config.GetFloat('makeatoon-sfx-volume', 1)
         self.soundBack = loader.loadSfx('phase_3/audio/sfx/GUI_create_toon_back.ogg')
