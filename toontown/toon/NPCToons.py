a0db9a5e0ff63a5a3687d5016da28312f9455b8e
Jaden <me@rocketprogrammer.me>
2026-09-11 19:58:46 -0500

    apply: NPCToons patch here

--- toontown/toon/NPCToons.py
+++ toontown/toon/NPCToons.py
@@ -11735,6 +11735,7 @@ def generateZone2NpcDict():
         zoneId = npcDesc[0]
         if zoneId in zone2NpcDict:
             zone2NpcDict[zoneId].append(id)
+            zone2NpcDict[zoneId].sort()
         else:
             zone2NpcDict[zoneId] = [id]
 
