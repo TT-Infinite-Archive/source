c14c48c79df6401dac34e160592ed668629b30ea
sypion <8906653+sypion@users.noreply.github.com>
2026-09-21 00:17:20 -0700

    fix(parties): use setBounds for the fireworks rocket since Panda3D 1.11 removed setBound

--- toontown/parties/DistributedPartyFireworksActivity.py
+++ toontown/parties/DistributedPartyFireworksActivity.py
@@ -56,7 +56,7 @@ class DistributedPartyFireworksActivity(DistributedPartyActivity, FireworkShowMi
         self.rocketActor = Actor('phase_13/models/parties/rocket_model', {'launch': 'phase_13/models/parties/rocket_launch'})
         rocketLocator = self.launchPadModel.find('**/rocket_locator')
         self.rocketActor.reparentTo(rocketLocator)
-        self.rocketActor.node().setBound(OmniBoundingVolume())
+        self.rocketActor.node().setBounds(OmniBoundingVolume())
         self.rocketActor.node().setFinal(True)
         effectsLocator = self.rocketActor.find('**/joint1')
         self.rocketExplosionEffect = RocketExplosion(effectsLocator, rocketLocator)
