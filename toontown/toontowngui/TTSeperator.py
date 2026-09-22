da7683828ee81e4b47097737f79b2b8cd8186589
sypion <8906653+sypion@users.noreply.github.com>
2026-09-21 20:10:03 -0700

    chore: remove modules nothing can load

--- toontown/toontowngui/TTSeperator.py
+++
@@ -1,9 +0,0 @@
-from direct.gui.DirectGui import DirectFrame, DGG
-from toontown.toonbase.ColorGlobals import CBlack
-
-
-class TTSeperator(DirectFrame):
-    def __init__(self, parent=aspect2d, width=0.002, length=0.8, pos=(0.0, 0.0, 0.0)):
-        frameSize = (-length, length, -width, width)
-        DirectFrame.__init__(self, parent, relief=None, pos=pos)
-        self.mainFrame = DirectFrame(self, relief=DGG.FLAT, frameSize=frameSize, frameColor=CBlack)
