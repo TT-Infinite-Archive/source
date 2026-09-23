64151ae1b58ecabac7601149a7704a0419fa3913
sypion <8906653+sypion@users.noreply.github.com>
2026-09-22 19:57:12 -0700

    chore(toonbase): remove the graphics settings method nothing calls

--- toontown/toonbase/ToonBase.py
+++ toontown/toonbase/ToonBase.py
@@ -1,4 +1,4 @@
-from panda3d.core import ConfigVariableBool, ConfigVariableDouble, ConfigVariableInt, ConfigVariableString, Connection, CullBinManager, DSearchPath, Filename, TextProperties, TextPropertiesManager, URLSpec, VBase4, VirtualFileSystem, WindowProperties, loadPrcFileData
+from panda3d.core import ConfigVariableBool, ConfigVariableDouble, ConfigVariableInt, ConfigVariableString, Connection, CullBinManager, DSearchPath, Filename, TextProperties, TextPropertiesManager, URLSpec, VBase4, VirtualFileSystem, WindowProperties
 import fractions
 import os
 import random
@@ -731,15 +731,6 @@ class ToonBase(OTPBase.OTPBase):
 
         res = resolutions[0]
         return res
-    
-    def updateGraphicsSettings(self):
-        '''
-        Reloads graphics settings
-        '''
-        loadPrcFileData('Settings: Texture Quality',
-                'max-texture-dimension %d' % SettingsGlobals.TextureOptionToDimension[settings.get(SettingsGlobals.TextureQuality)])
-        loadPrcFileData('Settings: Texture Compression',
-                'compressed-textures #%s' % 't' if settings[SettingsGlobals.CompressTextures] else 'f')
 
 
 @magicWord(category=CATEGORY_ADMINISTRATOR, types=[int])
